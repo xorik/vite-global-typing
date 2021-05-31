1c9d34c5c3c8f280465a8a1ac4663cf94f5c6311
xorik <xorik@users.noreply.github.com>
2021-05-31 20:34:23 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Button :size="123"></Button>
+</template>
+
+<script setup lang="ts">
+
+import Button from './components/Button.vue'
+</script>
+
---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>Hello</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    required: true,
+  },
+})
+</script>
